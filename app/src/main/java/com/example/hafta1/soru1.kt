6bfb86b63b5eae7e8fe20efb21e516d319b61d14
nisanursrc <nisanurrsrc4@gmail.com>
2023-07-20 16:14:23 +0300

    first commit

---
+++ app/src/main/java/com/example/hafta1/soru1.kt
@@ -0,0 +1,26 @@
+package com.example.hafta1
+
+import java.util.Scanner
+
+fun main() {
+    // İlk kullanıcı adı ve şifreyi tanımlıyoruz
+    val kullaniciAdi = "süRücü"
+    val sifre = "sRc444"
+
+    // Kullanıcıdan giriş bilgilerini almak için Scanner kullanıyoruz
+    val scanner = Scanner(System.`in`)
+
+    // Kullanıcı adı ve şifreyi giriş isteğiyle alıyoruz
+    println("Kullanıcı adı:")
+    val girilenKullaniciAdi = scanner.nextLine()
+
+    println("Şifre:")
+    val girilenSifre = scanner.nextLine()
+
+    // Kullanıcı adı ve şifre kontrolü
+    if (girilenKullaniciAdi.equals(kullaniciAdi, ignoreCase = true) && girilenSifre.equals(sifre, ignoreCase = true)) {
+        println("Hoş Geldiniz!!")
+    } else {
+        println("Kullanıcı adı veya şifre hatalı")
+    }
+}
---
+++ app/src/main/java/com/example/hafta1/soru2.kt
@@ -0,0 +1,21 @@
+package com.example.hafta1
+
+fun main() {
+    try {
+        print("Yaşınızı giriniz: ")
+        val input = readLine()
+        if (input != null) {
+            val yas = input.toInt()
+
+            if (yas >= 18) {
+                println("Oy kullanabilirsiniz!")
+            } else {
+                println("Oy kullanamazsınız.")
+            }
+        } else {
+            println("Geçerli bir yaş girmediniz.")
+        }
+    } catch (e: NumberFormatException) {
+        println("Geçerli bir yaş girmediniz.")
+    }
+}
